75d5e4cdaf69c601c7547c066a815b59bf5cd5be
DESKTOP-VVQTE10\MIRO <vasilevm4@gmail.com>
2021-01-29 18:12:46 +0200

    updated

---
+++ ProgrammingAdvancedC#/Defining Classes - Lab/1. Car/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine (int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+    }
+}
---
+++ ProgrammingAdvancedC#/Defining Classes - Lab/1. Car/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        { Engine v12 = new Engine(560,6300);
+            var tires = new Tire[4]
+                {
+                    new Tire(2018, 2.5),
+                    new Tire(2018, 2.1),
+                    new Tire(2018, 0.5),
+                    new Tire(2018, 2.3),
+
+            };
+
+            Car bmw = new Car("BMW", "X6", 1993, 5000, -50, v12, tires);
+
+            Console.WriteLine("Horse power: " + bmw.Engine.HorsePower);
+
+
+            foreach (var tire in bmw.Tires)
+            {
+                Console.WriteLine($"{tire.Year} - {tire.Pressure}");
+            }
+
+
+
+
+
+            //Car defaultGolf = new Car();
+            //Console.WriteLine($"Default golf: " + defaultGolf.WhoAmI());
+            //Car car = new Car();
+
+            //car.Make = "VW";
+            //car.Model = "MK3";
+            //car.Year = 1992;
+            //car.FuelQuantity = 200;
+            //car.FuelConsumption = 200;
+
+            //car.Drive(2000);
+
+            //Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
