1deda78bc0c93d2f70f2a7509fc77df892e5b0d4
DESKTOP-VVQTE10\MIRO <vasilevm4@gmail.com>
2021-02-05 21:19:18 +0200

    updated

---
+++ ProgrammingAdvancedC#/Generics - Lab/ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+   public class ArrayCreator
+    {
+
+       public static T[] Create<T>(int lenght, T defaultItem)
+        {
+            T[] array = new T[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = defaultItem;
+            }
+            return array;
+        }
+    }
+}
---
+++ ProgrammingAdvancedC#/Generics - Lab/ArrayCreator/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] array = ArrayCreator.Create(5,"Gogi");
+
+            Console.WriteLine(string.Join(",", array));
+        }
+    }
+}
