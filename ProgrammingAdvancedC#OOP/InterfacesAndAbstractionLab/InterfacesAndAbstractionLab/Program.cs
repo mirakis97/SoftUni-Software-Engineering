6834c6eaf4d0c404c7d8b4e705f7e475128f41ee
DESKTOP-VVQTE10\MIRO <vasilevm4@gmail.com>
2021-03-02 20:52:26 +0200

    Lab

---
+++ ProgrammingAdvancedC#OOP/InterfacesAndAbstractionLab/InterfacesAndAbstractionLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesAndAbstractionLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
