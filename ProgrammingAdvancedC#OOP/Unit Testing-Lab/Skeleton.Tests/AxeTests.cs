63cac89bcb329826efa7eee1d17c80e5680534da
DESKTOP-VVQTE10\MIRO <vasilevm4@gmail.com>
2021-03-19 21:06:38 +0200

    Lab

---
+++ ProgrammingAdvancedC#OOP/Unit Testing-Lab/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton.Tests
+{
+    [TestClass]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLoosesDurabilityAfterAttack()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            NUnit.Framework.Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability dosen`t change after attack.");
+        }
+        [Test]
+        public void BrokenAxeCantAttack()
+        {
+            Axe axe = new Axe(1, 1);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            NUnit.Framework.Assert.That(() => axe.Attack(dummy),
+              Throws.InvalidOperationException
+                .With.Message.EqualTo("Axe is broken."));
+        }
+
+
+    }
+}
---
+++ ProgrammingAdvancedC#OOP/Unit Testing-Lab/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Assert = NUnit.Framework.Assert;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private const int hp = 10;
+        private const int exp = 10;
+        private const int atk = 10;
+
+        Dummy dummy;
+
+
+        [SetUp]
+        public void Init()
+        {
+            dummy = new Dummy(10, 10);
+        }
+
+        [Test]
+        public void DummyLoosesHealthAfterAttack()
+        {
+
+
+            dummy.TakeAttack(atk);
+
+            Assert.That(dummy.Health, Is.EqualTo(0), "Dummy Healt doesn't change after attack");
+        }
+
+
+        [Test]
+        public void DummyThrowsExceptionIfAttacked()
+        {
+
+
+            dummy.TakeAttack(atk);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(atk));
+
+            Assert.That(ex.Message, Is.EqualTo("Dummy is dead."));
+
+        }
+
+
+        [Test]
+        public void DummyCanGiveExperience()
+        {
+            var hero = new Hero("Pepi");
+
+            hero.Attack(dummy);
+
+            Assert.That(hero.Experience, Is.EqualTo(10), "Dummy doesn't give experience when dead");
+        }
+
+        [Test]
+        public void AliveDummyCantGiveExperience()
+        {
+            var hero = new Hero("Mirko");
+
+            dummy = new Dummy(20, 10);
+
+            hero.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>((() => dummy.GiveExperience()));
+
+            Assert.That(ex.Message, Is.EqualTo("Target is not dead."));
+        }
+    }
+}
