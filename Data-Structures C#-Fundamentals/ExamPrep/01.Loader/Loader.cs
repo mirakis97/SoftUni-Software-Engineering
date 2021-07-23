38f7701bc27140d2873db1f291b7652fb8ceaddf
DESKTOP-VVQTE10\MIRO <vasilevm4@gmail.com>
2021-07-23 18:20:13 +0300

    ExamPrep

---
+++ Data-Structures C#-Fundamentals/ExamPrep/01.Loader/Loader.cs
@@ -0,0 +1,160 @@
+﻿namespace _01.Loader
+{
+    using _01.Loader.Interfaces;
+    using _01.Loader.Models;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Loader : IBuffer
+    {
+        private List<IEntity> entities;
+        public Loader()
+        {
+            entities = new List<IEntity>();
+        }
+        public int EntitiesCount => entities.Count;
+
+        public void Add(IEntity entity)
+        {
+            entities.Add(entity);
+        }
+
+        public void Clear()
+        {
+            entities.Clear();
+        }
+
+        public bool Contains(IEntity entity)
+        {
+            return entities.Contains(entity);
+        }
+
+        public IEntity Extract(int id)
+        {
+            IEntity entity = FindById(id);
+            if (entity != null)
+            {
+                 entities.Remove(entity);
+            }
+
+            return entity;
+        }
+
+        public IEntity Find(IEntity entity)
+        {
+            return FindByEntity(entity);
+        }
+
+        public List<IEntity> GetAll()
+        {
+            return new List<IEntity>(entities);
+        }
+
+        public IEnumerator<IEntity> GetEnumerator()
+        {
+            return entities.GetEnumerator();
+        }
+
+        public void RemoveSold()
+        {
+            List<IEntity> withoutSold = new List<IEntity>();
+
+            for (int i = 0; i < entities.Count; i++)
+            {
+                if (entities[i].Status != BaseEntityStatus.Sold)
+                {
+                    withoutSold.Add(entities[i]);
+                }
+            }
+            entities =  withoutSold;
+        }
+
+        public void Replace(IEntity oldEntity, IEntity newEntity)
+        {
+            int firstEntitiyIndex = entities.IndexOf(oldEntity);
+
+            CheckValidIndex(firstEntitiyIndex, "Entity not found");
+
+            entities[firstEntitiyIndex] = newEntity;
+        }
+
+        public List<IEntity> RetainAllFromTo(BaseEntityStatus lowerBound, BaseEntityStatus upperBound)
+        {
+            List<IEntity> inBount = new List<IEntity>();
+
+            for (int i = 0; i < entities.Count; i++)
+            {
+                if (entities[i].Status >= lowerBound && entities[i].Status <= upperBound)
+                {
+                    inBount.Add(entities[i]);
+                }
+            }
+            return inBount;
+        }
+
+        public void Swap(IEntity first, IEntity second)
+        {
+            int firstEntitiyIndex = entities.IndexOf(first);
+            int secondEntitiyIndex = entities.IndexOf(second);
+
+            CheckValidIndex(firstEntitiyIndex, "Entity not found");
+            CheckValidIndex(secondEntitiyIndex, "Entity not found");
+
+            IEntity temp = entities[firstEntitiyIndex];
+            entities[firstEntitiyIndex] = entities[secondEntitiyIndex];
+            entities[secondEntitiyIndex] = temp;
+        }
+
+        public IEntity[] ToArray()
+        {
+            return entities.ToArray();
+        }
+
+        public void UpdateAll(BaseEntityStatus oldStatus, BaseEntityStatus newStatus)
+        {
+            for (int i = 0; i < entities.Count; i++)
+            {
+                if (entities[i].Status == oldStatus)
+                {
+                    entities[i].Status = newStatus;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return entities.GetEnumerator();
+        }
+        private IEntity FindById(int id)
+        {
+
+            for (int i = 0; i < entities.Count; i++)
+            {
+                if (entities[i].Id == id)
+                {
+                    return entities[i];
+                }
+            }
+
+            return null;
+        }
+        private IEntity FindByEntity(IEntity entity)
+        {
+            int index = entities.IndexOf(entity);
+            if (index >= 0)
+            {
+                return entities[index];
+            }
+
+            return null;
+        }
+        private void CheckValidIndex(int index, string message)
+        {
+            if (index < 0)
+            {
+                throw new InvalidOperationException(message);
+            }
+        }
+    }
+}
---
+++ Data-Structures C#-Fundamentals/ExamPrep/02.Data/Data.cs
@@ -0,0 +1,99 @@
+﻿namespace _02.Data
+{
+    using _02.Data.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Data : IRepository
+    {
+        private PriorityQueue<IEntity> queue;
+        private Dictionary<int, IEntity> dictonary;
+
+        private Dictionary<int, List<IEntity>> parents;
+        public Data()
+        {
+            queue = new PriorityQueue<IEntity>();
+            dictonary = new Dictionary<int, IEntity>();
+            parents = new Dictionary<int, List<IEntity>>();
+        }
+        public Data(PriorityQueue<IEntity> queue, Dictionary<int, IEntity> dictionary, Dictionary<int, List<IEntity>> parents)
+        {
+            this.queue = queue;
+            this.dictonary = dictonary;
+            this.parents = parents;
+        }
+        public int Size => queue.Size;
+
+        public void Add(IEntity entity)
+        {
+            queue.Add(entity);
+            dictonary.Add(entity.Id, entity);
+            if (entity.ParentId != null)
+            {
+                if (!parents.ContainsKey((int)entity.ParentId))
+                {
+                    parents.Add((int)entity.ParentId, new List<IEntity>());
+                }
+                parents[(int)entity.ParentId].Add(entity);
+            }
+        }
+
+        public IRepository Copy()
+        {
+            return new Data(queue, dictonary, parents);
+        }
+
+        public IEntity DequeueMostRecent()
+        {
+            if (queue.Size == 0)
+            {
+                throw new InvalidOperationException("Operation on empty Data");
+            }
+            var element = queue.Dequeue();
+            dictonary.Remove(element.Id);
+            return element;
+        }
+
+        public List<IEntity> GetAll()
+        {
+            return queue.ToList;
+        }
+
+        public List<IEntity> GetAllByType(string type)
+        {
+            if (!(type == "User" || type == "StoreClient" || type == "Invoice"))
+            {
+                throw new InvalidOperationException($"Invalid type: " + type);
+            }
+            return queue.ToList.Where(e => e.GetType().Name == type).ToList();
+        }
+
+        public IEntity GetById(int id)
+        {
+            if (!dictonary.ContainsKey(id))
+            {
+                return null;
+            }
+            return dictonary[id];
+        }
+
+        public List<IEntity> GetByParentId(int parentId)
+        {
+            if (!parents.ContainsKey(parentId))
+            {
+                return new List<IEntity>();
+            }
+            return parents[parentId];
+        }
+
+        public IEntity PeekMostRecent()
+        {
+            if (queue.Size == 0)
+            {
+                throw new InvalidOperationException("Operation on empty Data");
+            }
+            return queue.Peek();
+        }
+    }
+}
