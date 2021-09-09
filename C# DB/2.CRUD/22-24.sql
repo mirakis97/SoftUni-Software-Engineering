cfe58e29eaf36aac2cd3362b5f453399bb75aadb
DESKTOP-VVQTE10\MIRO <vasilevm4@gmail.com>
2021-09-09 20:08:23 +0300

    Update

---
+++ C# DB/2.CRUD/22-24.sql
@@ -0,0 +1,11 @@
+--22. All Mountain Peaks
+SELECT PeakName FROM Peaks ORDER BY PeakName
+--23. Biggest Countries by Population
+SELECT TOP(30) CountryName,[Population] FROM Countries WHERE ContinentCode = 'EU'  ORDER BY [Population] DESC,CountryName
+--24. Countries and Currency (Euro / Not Euro)
+SELECT  CountryName,CountryCode,
+	CASE 
+		WHEN CurrencyCode = 'EUR' THEN 'Euro'
+		ELSE 'Not Euro'
+	END AS Currency
+FROM Countries ORDER BY CountryName ASC
---
+++ C# DB/2.CRUD/25.sql
@@ -0,0 +1,2 @@
+--25. All Diablo Characters
+SELECT [Name] FROM Characters ORDER BY [Name] ASC
