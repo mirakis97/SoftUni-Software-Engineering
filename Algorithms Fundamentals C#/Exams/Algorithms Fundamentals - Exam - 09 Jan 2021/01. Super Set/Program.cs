daa92f8d47445101b3ecc066fcbb15335cb01b92
Miroslav <vasilevm4@gmail.com>
2022-01-20 11:38:25 +0200

    Exam

---
+++ Algorithms Fundamentals C#/Exams/Algorithms Fundamentals - Exam - 09 Jan 2021/01. Super Set/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01._Super_Set
+{
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            var subset = Subsets(arr).Reverse();
+            foreach (var sub in subset)
+            {
+                Console.WriteLine(string.Join(" ",sub));
+            }
+        }
+        public static IList<IList<int>> Subsets(int[] nums)
+        {
+            if (nums == null) return null;
+
+            HashSet<string> unique = new HashSet<string>();
+            IList<IList<int>> lst = new List<IList<int>>();
+            if (nums.Length == 0)
+                return lst;
+            lst.Add(new int[0]);
+
+            Queue<int[]> q = new Queue<int[]>();
+            q.Enqueue(nums);
+
+            while (q.Count != 0)
+            {
+                int[] temp1 = q.Dequeue();
+                lst.Add(temp1);
+
+                if (temp1.Length > 1)
+                {
+                    for (int i = 0; i < temp1.Length; i++)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        int[] temp2 = new int[temp1.Length - 1];
+                        int counter = 0;
+                        for (int j = 0; j < temp1.Length; j++)
+                        {
+                            if (j != i)
+                            {
+                                temp2[counter++] = temp1[j];
+                                sb.Append(temp1[j].ToString() + "-");
+                            }
+                        }
+                        if (!unique.Contains(sb.ToString()))
+                        {
+                            unique.Add(sb.ToString());
+                            q.Enqueue(temp2);
+                        }
+                    }
+                }
+            }
+
+            return lst;
+        }
+    }
+}
---
+++ Algorithms Fundamentals C#/Exams/Algorithms Fundamentals - Exam - 09 Jan 2021/02. Paths/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Paths
+{
+    public class Program
+    {
+
+        private static List<int>[] graph;
+        private static bool[] visited;
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[n];
+            visited = new bool[n];
+            IList<IList<int>> set;
+            for (int node = 0; node < n; node++)
+            {
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    graph[node] = new List<int>();
+                }
+                else
+                {
+                    var childer = line.Split()
+                        .Select(int.Parse)
+                        .ToList();
+
+                    graph[node] = childer;
+                }
+                
+            }
+
+
+            set = AllPathsSourceTarget(graph);
+            foreach (var item in set)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+
+        }
+        public static IList<IList<int>> AllPathsSourceTarget(List<int>[] graph)
+        {
+
+            List<IList<int>> res = new List<IList<int>>();
+            if (graph == null || graph.Length == 0)
+                return res;
+
+            DFS(graph, res, new List<int>() { 0 }, 0);
+            DFS(graph, res, new List<int>() , 0);
+            DFS(graph, res, new List<int>(), 1);
+
+            return res;
+        }
+
+        private static void DFS(List<int>[] graph, List<IList<int>> res, List<int> path, int i)
+        {
+            if (i == graph.Length - 1)
+            {
+                res.Add(new List<int>(path));
+                return;
+            }
+
+            foreach (var next in graph[i])
+            {
+                path.Add(next);
+                DFS(graph, res, path, next);
+                path.RemoveAt(path.Count - 1);
+            }
+        }
+    }
+}
